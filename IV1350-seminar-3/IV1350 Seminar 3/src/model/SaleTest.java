d954b987b5a563a533364827595fb565dd02da65
Erik Vinblad <erik.j.winbladh@gmail.com>
2023-05-04 20:24:57 +0200

    Update SaleTest.java

--- IV1350-seminar-3/IV1350 Seminar 3/src/model/SaleTest.java
+++ IV1350-seminar-3/IV1350 Seminar 3/src/model/SaleTest.java
@@ -6,11 +6,9 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import integration.Printer;
 
 class SaleTest {
@@ -36,16 +34,15 @@ class SaleTest {
 
 		sale.addItem(item);
 		sale.addItem(item);
-
 		List<Item> itemList = sale.getItemList();
+		
 		assertEquals(2, itemList.size(), "Item should have been added to the sale");
 		assertEquals(item, itemList.get(0), "Item added to the sale should be the same as the one passed to addItem()");
 		assertEquals(item, itemList.get(1), "Item added to the sale should be the same as the one passed to addItem()");
 		assertEquals(2, sale.getItemQuantityMap().get(item));
 
 		double expectedTotalPrice = 2 * (item.getItemPrice() * (1 + item.getVAT()));
-		assertEquals(expectedTotalPrice, sale.getTotalPrice(),
-				"Total price should be updated correctly after calling addItem()");
+		assertEquals(expectedTotalPrice, sale.getTotalPrice(), "Total price should be updated correctly after calling addItem()");
 	}
 
 	@Test
@@ -60,9 +57,7 @@ class SaleTest {
 		sale.applyDiscount(discount);
 
 		double expectedTotalPrice = item.getItemPrice() * (1 + item.getVAT()) * (1 - discount);
-
-		assertEquals(expectedTotalPrice, sale.getTotalPrice(),
-				"Total price should be updated correctly after applying a discount");
+		assertEquals(expectedTotalPrice, sale.getTotalPrice(), "Total price should be updated correctly after applying a discount");
 	}
 
 	@Test
@@ -98,19 +93,3 @@ class SaleTest {
 
 	}
 }
-/**
- * @Test public void testGetters() {
- * 
- *       sale.applyDiscount(0.1); sale.pay(100);
- * 
- *       assertEquals(sale.getPayment(), 100, 0.001);
- * 
- *       assertEquals(sale.getPrinter(), printer);
- *       assertEquals(sale.getDiscount(), 0.1, 0.001);
- *       assertEquals(sale.getItemList(), new ArrayList<Item>());
- *       assertEquals(sale.getTotalPrice(), 0.0, 0.001); // the total price
- *       should be 0 before any items are added }
- * 
- *       }
- * 
- */
